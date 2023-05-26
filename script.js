9211661a2ffaefb143f3e5d7c93c5fd00a55bd86
Shreyas2007 <84057370+shreyas391@users.noreply.github.com>
2023-05-26 12:31:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+AOS.init();
+
+var gmail = document.getElementById("gmail");
+
+gmail.addEventListener("click", function() {
+    document.getElementById("address").innerHTML = "shreyashema2007@gmail.com"
+});
+
