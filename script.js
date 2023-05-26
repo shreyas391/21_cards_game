139c68d86398f328f56e33aa94743dadb54a3fb5
Shreyas2007 <84057370+shreyas391@users.noreply.github.com>
2023-05-26 12:31:41 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-AOS.init();
-
-var gmail = document.getElementById("gmail");
-
-gmail.addEventListener("click", function() {
-    document.getElementById("address").innerHTML = "shreyashema2007@gmail.com"
-});
-
